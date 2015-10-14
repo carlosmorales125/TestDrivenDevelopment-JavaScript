9744f0c27c247be04598a4cfcdb893676e90ba68
carlosmorales125 <carlos.rey.morales@gmail.com>
2015-10-14 14:13:12 -0400

    Adding package.json file and added test to the project.

--- spec/returnFirstFromArraySpec.js
+++ spec/returnFirstFromArraySpec.js
@@ -1,5 +1,23 @@
-describe('returnsTrue', function(){
-	it('should return true', function(){
-		expect(returnsTrue()).toBe(true);
+describe('returnFirstFromArray', function(){
+	var first = [];
+
+	it('should return the first object in array', function(){
+		expect(returnFirstFromArray([1,2,3])).toBe(1);
+	});
+
+	it('should return the first object in array', function(){
+		expect(returnFirstFromArray(['a','b','c'])).toBe('a');
+	});
+
+	it('should return the first object in array', function(){
+		expect(returnFirstFromArray([first,'a','b','c'])).toBe(first);
+	});
+
+	it('should return the first object in array', function(){
+		expect(returnFirstFromArray([undefined,'a','b','c'])).toBe(undefined);
+	});
+
+	it('should return undefined for an empty array', function(){
+		expect(returnFirstFromArray([])).toBe(undefined);
 	});
 });
---
+++ spec/returnsTrueSpec.js
@@ -0,0 +1,6 @@
+//This test confirms that all files have been loaded into the browsers by karma.
+describe('returnsTrue', function(){
+	it('should return true', function(){
+		expect(returnsTrue()).toBe(true);
+	});
+});
